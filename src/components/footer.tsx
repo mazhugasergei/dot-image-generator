6c4685b17feedc1c78cc5377626f9d82f057c61c
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2026-02-05 17:37:51 +1000

    fix: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,12 +5,12 @@ export function Footer() {
 		<footer className="text-muted-foreground text-center text-sm">
 			Made with <HeartIcon className="fill-foreground text-foreground mx-1 inline-block size-4" /> by{" "}
 			<a
-				href="https://github.com/msergius"
+				href="https://github.com/mazhugasergei"
 				target="_blank"
 				rel="noopener noreferrer"
 				className="text-foreground font-medium underline"
 			>
-				msergius
+				mazhugasergei
 			</a>
 		</footer>
 	)
